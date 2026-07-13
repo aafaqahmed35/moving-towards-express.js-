4c4ea49538749b173194062ca694682e34d79b67
veil_strider42 <aafaqahmedsma@gmail.com>
2026-07-13 16:09:09 +0530

    routing established

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
-// Import the Express framework
-const express = require('express');
+const express=require('express');
 
-// Create an Express application
-const app = express();
+const app= express();
 
+app.use('/products', (req,res,next)=>{
+    console.log("Currently in products page");
+    res.send('<html><b><h1>PRODUCTS PAGE</h1></b></html>');
+});
 
-// Middleware 1
-// This runs for every incoming request because no path is specified.
-app.use((req, res, next) => {
-    console.log('In the middleware');
-
-    // Pass control to the next middleware in the chain
-    next();
+app.use('/', (req,res,next)=>{
+    console.log("Currently in home page");
+    res.send('<html><b><h1>HOME PAGE</h1></b></html>');
 });
 
 
-// Middleware 2
-// This also runs for every request after Middleware 1 calls next().
-app.use((req, res, next) => {
-    console.log('In the another middleware');
 
-    // Pass control to the next middleware.
-    // Since there is no more middleware or route handler after this,
-    // the request will eventually hang because no response is sent.
-    next();
-});
-
-
-// Start the server and listen for incoming requests on port 3000
 app.listen(3000);
